f6eaa764bbe508e2fac9b23bb2d0dd5d2fd12358
Sirine Dridi <sirine@live.unc.edu>
2020-09-15 09:39:15 -0400

    Initial commit

---
+++ src/a4/Main.java
@@ -0,0 +1,14 @@
+package a4;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.removeAtIndex(0);
+        System.out.println(list.toString());
+    }
+}
